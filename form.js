267329f1d2422ef30c24720c1146f7931fffdffa
prabhakarkumar17 <42499823+prabhakarkumar17@users.noreply.github.com>
2019-08-16 17:14:26 +0530

    Update form.js

--- form.js
+++ form.js
@@ -1,11 +1,7 @@
 function validate() {
     var a = document.getElementById("userid").value;
     if (a == "") {
-        document.getElementById("msg").innerHTML = "**Please enter user name";
-        return false;
-    }
-    if ((a.length < 3) || (a.length > 25)) {
-        document.getElementById("msg").innerHTML = "**Please enter user name more than 3 characters";
+        document.getElementById("msg").innerHTML = "**User Name cant be blank";
         return false;
     }
     var b = document.getElementById("password").value;
