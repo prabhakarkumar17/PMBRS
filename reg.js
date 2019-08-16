7d4a0cc7c899672e006ea3dba45a7408a1c1b0e2
prabhakarkumar17 <42499823+prabhakarkumar17@users.noreply.github.com>
2019-08-16 17:12:03 +0530

    Update reg.js

--- reg.js
+++ reg.js
@@ -4,11 +4,8 @@ function validate() {
     var name = document.getElementById("name").value;
     if (name.length == "") {
         document.getElementById("error_name").innerHTML = "**name should not be blank";
+    }
 
-    }
-    if ((name.length < 3) || (name.length > 25)) {
-        document.getElementById("error_name").innerHTML = "**name should be within range of 3 to 25 words";
-    }
 
     // FOR EMAIL VALIDATION
     var text = document.getElementById("email").value;
@@ -34,61 +31,6 @@ function validate() {
 
     if (date.test(dat) == false) {
         document.getElementById("error_date").innerHTML = "**enter valid date";
-    }
-
-}
-
-function validate_name() {
-    //FOR NAME VALIDATION
-
-    var name = document.getElementById("name").value;
-    if (name.length == "") {
-        document.getElementById("error_name").innerHTML = "**name should not be blank";
-
-    } else if ((name.length < 3) || (name.length > 25)) {
-        document.getElementById("error_name").innerHTML = "**name should be within range of 3 to 25 words";
-    } else {
-        document.getElementById("error_name").innerHTML = "";
-    }
-}
-
-function validate_email() {
-
-    // FOR EMAIL VALIDATION
-    var text = document.getElementById("email").value;
-    var regx = /^([a-zA-Z0-9\.-]+)@([a-zA-Z]+).([a-z]{2,20})$/;
-
-    if (regx.test(text) == false) {
-        document.getElementById("error_email").innerHTML = "**enter valid email";
-    } else {
-        document.getElementById("error_email").innerHTML = "";
-    }
-}
-
-function validate_contact() {
-
-    //FOR CONTACT VALIDATION
-    var mobile = document.getElementById("contact_no").value;
-    var contact = /^[6-9]\d{9}$/;
-
-    if (contact.test(mobile) == false) {
-        document.getElementById("error_contact").innerHTML = "**enter valid contact no";
-
-    } else {
-        document.getElementById("error_contact").innerHTML = " ";
-    }
-}
-
-function validate_date() {
-
-    // FOR DATE VALIDATION
-    var dat = document.getElementById("date").value;
-    var date = /^([1-31])([1-12])([1990-2030])$/;
-
-    if (date.test(dat) == false) {
-        document.getElementById("error_date").innerHTML = "**enter valid date";
-    } else {
-        document.getElementById("error_date").innerHTML = " ";
     }
 
 }
